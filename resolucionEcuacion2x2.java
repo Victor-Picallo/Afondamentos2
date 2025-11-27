d4ca12214beb9bdbbb593a0b9be070877912179a
Victor-Picallo <victor.p9646@gmail.com>
2025-11-27 10:50:06 +0100

    cosas nuevas

--- resolucionEcuacion2x2.java
+++ resolucionEcuacion2x2.java
@@ -1,19 +1,15 @@
 package Ecuaciones;
-public class resolucionEcuacion2x2 {
 
+public class resolucionEcuacion2x2 {
 
     public static void main(String[] args) {
 
-
-
         ecuaciones ec = new ecuaciones();
-
 
         double[][] Matriz = ec.pedirMatrizCoeficientes2x2();
         double[] TerminosIndependientes = ec.pedirTerminosIndependientes2x1();
 
         ec.mostrarSoluciones(Matriz, TerminosIndependientes);
-
 
     }
 }
